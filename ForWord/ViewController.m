d01a8205577f3c5fef94ad59fb3666ecb25681c4
PingAnXL <PingAnXL@PingAnXLdeMacBook-Pro.local>
2017-08-14 17:07:47 +0800

    使用objc_msgSend

--- ForWord/ViewController.m
+++ ForWord/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <objc/message.h>
 
 #import "OBGManager.h"
 
@@ -18,26 +19,106 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
 }
+
+
+// oc 中的创建对象和初始化对象两种写法
+- (void)creatNSObjectTest {
+
+    // 下面的可以代表  [[NSObject alloc] init];
+    /* objc_msgSend 前半部分表示传的参数类型  id (*)  表示返回值的类型  objc_msgSend 后半部分和前半部分是一一对应的
+     *
+     */
+    // 1.创建对象
+    NSObject * p1 =  ((NSObject * (*)(id,SEL))objc_msgSend)((id)[NSObject class],@selector(alloc));
+    // 2.初始化对象
+    p1 =  ((NSObject *(*)(id, SEL))objc_msgSend)((id)p1,@selector(init));
+
+}
+
+
+// 使用 objc_msgSend 发送 无参数和无返回值消息
+- (void)testObjc {
+
+    ((void (*)(id,SEL))objc_msgSend)(self,@selector(test1));
+
+}
+// 方法的实现
+- (void)test1 {
+
+    NSLog(@"无参数无返回值");
+
+}
+
+// 有参数 无返回值
+- (void)testObjcMessage:(id)arg{
+
+    
+    ((void (*)(id,SEL,id))objc_msgSend)(self,@selector(test1:),arg);
+
+}
+// 方法的实现
+- (void)test1:(id)arg {
+
+    NSLog(@"%@",arg);
+
+}
+
+// 无参数有返回值
+- (id)testObjcMessage {
+
+    return  ((id (*)(id, SEL))objc_msgSend)(self,@selector(test3));
+
+}
+// 方法的实现
+- (id)test3 {
+
+ return @"有返回值";
+
+}
+
+// 有返回值 有参数
+- (id)testMessageObjcSend:(id)arg {
+
+    return ((id (*) (id,SEL,id))objc_msgSend)(self,@selector(test4:),arg);
+
+}
+//方法的实现
+- (id)test4:(id)arg {
+
+    return arg;
+}
+
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
 
 
-        NSDictionary * dic = @{@"name":@"qweqwe",@"age":@"10"};
+//        NSDictionary * dic = @{@"name":@"qweqwe",@"age":@"10"};
+//    
+//        [OBGManager requestAPIName:APINameTypeTest2 paraters:dic callBack:^(id response, id data) {
+//    
+//            NSLog(@"%@ %@",response,data);
+//    
+//        }];
     
-        [OBGManager requestAPIName:APINameTypeTest2 paraters:dic callBack:^(id response, id data) {
+    //无参数无返回值
+//    [self testObjc];
     
-            NSLog(@"%@ %@",response,data);
+    // 有参数 无返回值的
+//    [self testObjcMessage:@"test"];
     
+    // 无参数 有返回值
     
-            
-        }];
-
+//    id t = [self testObjcMessage];
+//    NSLog(@"%@",t);
+//
+    // 有返回值 有参数
+    id t2 = [self testMessageObjcSend:@"有返回值，有参数的"];
+    
+    NSLog(@"%@",t2);
 
 }
-
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
